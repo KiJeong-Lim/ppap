6f2f24b201e855bd4cb7fef23b373be6044f57f5
KiJeong-Lim <gijungdduk@naver.com>
2021-09-26 07:10:16 +0900

    .

---
+++ README.md
@@ -0,0 +1,13 @@
+# ppap
+
+## Aladdin
+
+- My dialect of lambda-Prolog
+
+## LGS
+
+- My lexer generator
+
+## PGS
+
+- My parser generator
--- src/Z/Text/PC/Test.hs
+++ src/Z/Text/PC/Test.hs
@@ -43,12 +43,12 @@ instance EqProp val => EqProp (MyPC val) where
     p1 =-= p2 = execPC p1 =-= execPC p2
 
 checkParserBaseIsMonad :: TestBatch
-checkParserBaseIsMonad = ("check-`ParserBase'-is-`Monad'", map (fmap (withMaxSuccess 50000)) (snd (go undefined))) where
+checkParserBaseIsMonad = ("check \"Monad ParserBase\"", map (fmap (withMaxSuccess 50000)) (snd (go undefined))) where
     go :: PB LocChr (Int, Int, Int) -> TestBatch
     go = monad
 
 checkParserBaseIsMonadPlus :: TestBatch
-checkParserBaseIsMonadPlus = ("check-`ParserBase'-is-`MonadPlus'", map (fmap (withMaxSuccess 50000)) (snd (go undefined))) where
+checkParserBaseIsMonadPlus = ("check \"MonadPlus ParserBase\"", map (fmap (withMaxSuccess 50000)) (snd (go undefined))) where
     go :: PB LocChr (Int, Int) -> TestBatch
     go = monadPlus
 
