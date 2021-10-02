c6716e3d82586fa574e133d35d8adbe360ecfe94
KiJeong-Lim <gijungdduk@naver.com>
2021-10-02 16:57:21 +0900

    .

--- src/Main.hs
+++ src/Main.hs
@@ -9,46 +9,72 @@ import Z.System.Shelly
 import Z.Utils
 
 showCopyright :: String
-showCopyright = concat
-    [ "Copyright (c) 2020-2021, Kijeong Lim\n"
-    , "All rights reserved.\n"
+showCopyright = unlines
+    [ "Copyright (c) 2020-2021, Kijeong Lim"
+    , "All rights reserved."
     ]
+
+extractArgs :: String -> Maybe [String]
+extractArgs args_rep
+    | take 2 args_rep == "--"
+    = case span (\ch -> ch /= ' ') (drop 2 args_rep) of
+        (arg, next_args_rep) -> do
+            next_args <- extractArgs next_args_rep
+            return (arg : next_args)
+    | otherwise
+    = case args_rep of
+        [] -> return []
+        ' ' : next_args_rep -> extractArgs next_args_rep
+        _ -> Nothing
+
+matchCommand :: String -> Maybe (String, [String])
+matchCommand str
+    | null str = return ("", [])
+    | otherwise = foldMap matchPrefix ["Aladdin", "Calc", "LGS", "PGS", "TEST"]
+    where
+        matchPrefix :: String -> Maybe (String, [String])
+        matchPrefix pre = do
+            let n = length pre
+            args <- if pre == take n str
+                then extractArgs (drop n str)
+                else Nothing
+            return (pre, args)
 
 ppap :: IO ()
 ppap = do
     command <- shelly "ppap =<< "
-    case command of
-        "" -> do
+    case matchCommand command of
+        Just ("", []) -> do
             shelly "ppap >>= quit"
             return ()
-        "Aladdin" -> do
+        Just ("Aladdin", []) -> do
             shelly "ppap >>= exec (Aladdin.main)"
             Aladdin.main
-        "Calc" -> do
+        Just ("Calc", []) -> do
             shelly "ppap >>= exec (Calc.main)"
             Calc.main
-        "Calc --quick" -> do
+        Just ("Calc", ["quick"]) -> do
             shelly "ppap >>= eval (Calc.runCalc \"calc-example1.calc\")"
             Calc.runCalc "calc-example1.calc"
             shelly "ppap >>= quit"
             return ()
-        "LGS" -> do
+        Just ("LGS", []) -> do
             shelly "ppap >>= exec (LGS.main)"
             LGS.main
-        "LGS --quick" -> do
+        Just ("LGS", ["quick"]) -> do
             shelly "ppap >>= eval (LGS.runLGS \"src/Aladdin/Front/Analyzer/Lexer\")"
             LGS.runLGS "src/Aladdin/Front/Analyzer/Lexer"
             shelly "ppap >>= quit"
             return ()
-        "PGS" -> do
+        Just ("PGS", []) -> do
             shelly "ppap >>= exec (PGS.main)"
             PGS.main
-        "PGS --quick" -> do
+        Just ("PGS", ["quick"]) -> do
             shelly "ppap >>= eval (PGS.runPGS \"src/Aladdin/Front/Analyzer/Parser\")"
             PGS.runPGS "src/Aladdin/Front/Analyzer/Parser"
             shelly "ppap >>= quit"
             return ()
-        "TEST" -> do
+        Just ("TEST", []) -> do
             shelly "ppap >>= exec (TEST.main)"
             TEST.main
         invalid_command -> do
@@ -57,4 +83,7 @@ ppap = do
             return ()
 
 main :: IO ()
-main = ppap
+main = do
+    putStrLn showCopyright
+    ppap
+    return ()
--- src/Z/Algo/Function.hs
+++ src/Z/Algo/Function.hs
@@ -3,4 +3,4 @@ module Z.Algo.Function where
 import Data.Function
 
 recNat :: Int -> a -> (Int -> a -> a) -> a
-recNat n init step = if n < 0 then undefined else foldr step init (reverse [0 .. n - 1])
+recNat n init step = foldr step init (reverse [0 .. n - 1])
