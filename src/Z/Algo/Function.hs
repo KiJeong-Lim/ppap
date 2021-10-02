e623a157930ad0dd5a284019491a7bb096062482
KiJeong-Lim <gijungdduk@naver.com>
2021-10-03 08:31:52 +0900

    .

--- src/Z/Algo/Function.hs
+++ src/Z/Algo/Function.hs
@@ -8,19 +8,14 @@ infix 9 />
 
 type ErrMsgM = Either String
 
-class TransErr a where
-    tryWith :: a -> a -> a
+class Failable a where
+    failed :: a -> Bool
 
-instance TransErr (Maybe a) where
-    tryWith (Nothing) = id
-    tryWith x = const x
+instance Failable (Maybe a) where
+    failed = Maybe.isNothing
 
-instance TransErr (Either e a) where
-    tryWith (Left _) = id
-    tryWith x = const x
-
-instance TransErr [a] where
-    tryWith xs ys = if null xs then ys else xs
+instance Failable (Either e a) where
+    failed = either (const True) (const False)
 
 recNat :: Int -> a -> (Int -> a -> a) -> a
 recNat n init step = foldr step init (reverse [0 .. n - 1])
@@ -37,8 +32,8 @@ fromErrMsgM = either error id
 addErrMsg :: String -> Maybe a -> ErrMsgM a
 addErrMsg str = Maybe.maybe (Left str) Right
 
-(/>) :: TransErr a => a -> a -> a
-x /> y = tryWith x y
+(/>) :: Failable a => a -> a -> a
+x /> y = if failed x then y else x
 
 safehd :: [a] -> Maybe a
-safehd = foldr (const . Just) Nothing . take 1
+safehd = foldr (const . Just) Nothing
