bfdc1d49df793cb5ab4ecc9442a39d11dfa9f86e
KiJeong-Lim <gijungdduk@naver.com>
2021-10-03 07:31:32 +0900

    .

--- src/Z/System/Path.hs
+++ src/Z/System/Path.hs
@@ -1,8 +1,6 @@
 module Z.System.Path where
 
-import qualified System.Path as Path
-import qualified System.Path.Directory as Directory
-import qualified System.Path.Windows as Windows
+import qualified System.Directory as Directory
 
 matchFileDirWithExtension :: String -> (String, String)
 matchFileDirWithExtension dir
@@ -10,19 +8,12 @@ matchFileDirWithExtension dir
         (reversed_extension, '.' : reversed_filename) -> (reverse reversed_filename, '.' : reverse reversed_extension)
         (reversed_filename, must_be_null) -> (reverse reversed_filename, [])
 
-toWindowsAbsPath :: String -> IO (Maybe String)
-toWindowsAbsPath = maybe (return Nothing) (fmap toString . Directory.canonicalizePath) . fromString where
-    toString :: Windows.AbsDir -> Maybe String
-    toString = uncurry go . span (\ch -> ch /= ':') . Windows.toString where
-        go :: String -> String -> Maybe String
-        go drive path
-            | take 3 path == drivesuffix = return (drive ++ path)
-            | take 2 path == take 2 drivesuffix = return (drive ++ drivesuffix ++ drop 2 path)
-            | otherwise = Nothing
-        drivesuffix :: String
-        drivesuffix = ":\\\\"
-    fromString :: String -> Maybe Windows.RelDir
-    fromString = Windows.maybe
-
-makePathAbsolutely :: String -> IO (Maybe String)
-makePathAbsolutely = toWindowsAbsPath
+makePathAbsolutely :: FilePath -> IO (Maybe FilePath)
+makePathAbsolutely = fmap (uncurry go . span (\ch -> ch /= ':')) . Directory.makeAbsolute where
+    go :: String -> String -> Maybe String
+    go drive path
+        | take 3 path == drivesuffix = return (drive ++ path)
+        | take 2 path == take 2 drivesuffix = return (drive ++ drivesuffix ++ drop 2 path)
+        | otherwise = Nothing
+    drivesuffix :: String
+    drivesuffix = ":\\\\"
