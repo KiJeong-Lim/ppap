36a344434f2093393f3acea20ccfb40aa3df65fb
KiJeong-Lim <gijungdduk@naver.com>
2021-10-04 02:48:02 +0900

    .

--- src/Z/Text/PC/Test.hs
+++ src/Z/Text/PC/Test.hs
@@ -4,29 +4,24 @@ import Control.Applicative
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
+import Z.Algo.Function
 import Z.Text.PC
 import Z.Text.PC.Base
 import Z.Text.PC.Internal
 import Z.Utils
 
 instance (Arbitrary chr, CoArbitrary chr, Arbitrary val) => Arbitrary (ParserBase chr val) where
-    arbitrary = choose (0, 10) >>= go where
-        go :: (Arbitrary chr, CoArbitrary chr, Arbitrary val) => Int -> Gen (ParserBase chr val)
-        go rank = if rank > 0
-            then frequency
-                [ (8, go (rank - 1))
-                , (2, pure PAct <*> genPAct (go (rank - 1)))
-                ]
-            else pure PVal <*> arbitrary
-        genPAct :: (Arbitrary chr, CoArbitrary chr) => Gen (ParserBase chr val) -> Gen ([chr] -> [(ParserBase chr val, [chr])])
-        genPAct seed = do
-            conds <- listOf arbitrary
-            ps <- listOf seed
-            return (parsing ps conds)
-        parsing :: [val] -> [chr -> Bool] -> ([chr] -> [(val, [chr])])
-        parsing vals [] str = [ (val, str) | val <- vals ]
-        parsing vals (cond : conds) (ch : str) = if cond ch then parsing vals conds str else []
-        parsing vals _ _ = []
+    arbitrary = choose (0, 10) >>= recNat myInit myStep where
+        myInit :: (Arbitrary val) => Gen (ParserBase chr val)
+        myInit = PVal <$> arbitrary
+        myStep :: (Arbitrary chr, CoArbitrary chr, Arbitrary val) => Int -> Gen (ParserBase chr val) -> Gen (ParserBase chr val)
+        myStep n prev = frequency
+            [ (8, prev)
+            , (2, PAct <$> (pure parsing <*> listOf prev <*> listOf arbitrary))
+            ]
+        parsing :: [p] -> [chr -> Bool] -> ([chr] -> [(p, [chr])])
+        parsing ps (cond : conds) (ch : str) = if cond ch then parsing ps conds str else []
+        parsing ps conds str = if null conds then [ (p, str) | p <- ps ] else []
     shrink = const []
 
 instance Show (ParserBase chr val) where
@@ -36,12 +31,12 @@ instance (Show chr, Arbitrary chr, EqProp val, EqProp chr) => EqProp (ParserBase
     p1 =-= p2 = execPB p1 =-= execPB p2
 
 checkParserBaseIsMonad :: TestBatch
-checkParserBaseIsMonad = (">>> checking=\"Monad (PB LocChr)\"", map (fmap (withMaxSuccess 100000)) (snd (go undefined))) where
+checkParserBaseIsMonad = (">>> checking=\"Monad (ParserBase LocChr)\"", map (fmap (withMaxSuccess 100000)) (snd (go undefined))) where
     go :: PB LocChr (Int, Int, Int) -> TestBatch
     go = monad
 
 checkParserBaseIsMonadPlus :: TestBatch
-checkParserBaseIsMonadPlus = (">>> checking=\"MonadPlus (PB LocChr)\"", map (fmap (withMaxSuccess 100000)) (snd (go undefined))) where
+checkParserBaseIsMonadPlus = (">>> checking=\"MonadPlus (ParserBase LocChr)\"", map (fmap (withMaxSuccess 100000)) (snd (go undefined))) where
     go :: PB LocChr (Int, Int) -> TestBatch
     go = monadPlus
 
