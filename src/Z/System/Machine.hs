0f1555c440c46766017234921a1e712c4bba6c1c
KiJeong-Lim <gijungdduk@naver.com>
2021-10-03 20:50:12 +0900

    .

--- src/Z/System/Machine.hs
+++ src/Z/System/Machine.hs
@@ -1,13 +1,9 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Z.System.Machine where
 
-import qualified Foreign.C.Error as C
-import qualified Foreign.C.String as C
-import qualified Foreign.C.Types as C
+import qualified Foreign.C as C
 
 foreign import ccall "runMachine" c_runMachine :: C.CString -> IO C.CInt
 
 runMachine :: String -> IO Integer
-runMachine str = do
-    exit_code <- C.withCString str c_runMachine
-    return (toInteger exit_code)
+runMachine str = fmap toInteger (C.withCString str c_runMachine)
