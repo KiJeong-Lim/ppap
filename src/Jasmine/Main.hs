a3b4241cd1b72f6843b9526c0aa80ef30f26199b
KiJeong-Lim <gijungdduk@naver.com>
2021-10-16 23:06:32 +0900

    .

--- src/Jasmine/Main.hs
+++ src/Jasmine/Main.hs
@@ -1,9 +1,10 @@
 module Jasmine.Main where
 
 import Jasmine.PresburgerSolver
+import Z.System.Shelly
 
 testPresburger :: IO ()
-testPresburger = mapM_ (putStrLn . go . getCase) [1 .. 9] where
+testPresburger = mapM_ (shelly . go . getCase) [1 .. 9] where
     getCase :: Int -> Formula TermRep
     getCase 1 = (AllF 1 (AllF 2 (EqnF (Plus (IVar 1) (IVar 2)) (Plus (IVar 2) (IVar 1)))))
     getCase 2 = (AllF 1 (LeqF (IVar 1) (IVar 1)))
@@ -14,12 +15,14 @@ testPresburger = mapM_ (putStrLn . go . getCase) [1 .. 9] where
     getCase 7 = (AllF 1 (ImpF (LtnF (Zero) (IVar 1)) (LtnF (IVar 1) (Plus (IVar 1) (IVar 1)))))
     getCase 8 = (AllF 1 (AllF 2 (LeqF (IVar 1) (Plus (IVar 1) (IVar 2)))))
     getCase 9 = (AllF 1 (AllF 2 (LeqF (IVar 2) (Plus (IVar 1) (IVar 2)))))
+    check :: Formula TermRep -> Maybe Bool
+    check = destiny . eliminateQuantifier . fmap runTermRep
     go :: Formula TermRep -> String
-    go f = case destiny (eliminateQuantifier (fmap runTermRep f)) of
+    go f = case check f of
         Nothing -> ("Presburger> The formula \'" ++ shows f ("\' is " ++ "not " ++ "a " ++ "sentence."))
-        Just vf -> ("Presburger> The formula \'" ++ shows f ("\' is " ++ "a " ++ showvf vf "sentence."))
-    showvf :: Bool -> ShowS
-    showvf vf = showString (if vf then "true " else "false ")
+        Just b_ -> ("Presburger> The formula \'" ++ shows f ("\' is " ++ "a " ++ showsb b_ "sentence."))
+    showsb :: Bool -> ShowS
+    showsb b_ = showString (if b_ then "true " else "false ")
 
 main :: IO ()
 main = return ()
