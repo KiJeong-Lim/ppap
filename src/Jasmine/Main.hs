fa0a882fa40632191d1ec81a6d746ea2af571834
KiJeong-Lim <gijungdduk@naver.com>
2021-10-17 19:40:18 +0900

    .

--- src/Jasmine/Main.hs
+++ src/Jasmine/Main.hs
@@ -7,7 +7,7 @@ import Z.System.Shelly
 testPresburger :: IO ()
 testPresburger = mapM_ (shelly . analyze . getCase) [0 .. 10] where
     getCase :: Int -> MyPresburgerFormulaRep
-    getCase 0 = (AllF 1 (AllF 2 (LeqF (Plus (Succ Zero) (IVar 2)) (Plus (IVar 1) (IVar 2)))))
+    getCase 0 = (AllF 1 (AllF 2 (LeqF (Plus (Succ (Zero)) (IVar 2)) (Plus (IVar 1) (IVar 2)))))
     getCase 1 = (AllF 1 (AllF 2 (EqnF (Plus (IVar 1) (IVar 2)) (Plus (IVar 2) (IVar 1)))))
     getCase 2 = (AllF 1 (LeqF (IVar 1) (IVar 1)))
     getCase 3 = (AllF 1 (AllF 2 (NegF (ConF (LeqF (IVar 1) (IVar 2)) (GtnF (IVar 1) (IVar 2))))))
