5061ada10c8feda67658c402ae738098a8df5b8b
KiJeong-Lim <gijungdduk@naver.com>
2021-10-26 11:36:10 +0900

    .

--- src/Z/System/File.hs
+++ src/Z/System/File.hs
@@ -1,39 +1,39 @@
 module Z.System.File where
 
+import Control.Monad
 import System.IO
 import Z.Utils
 
 readFileNow :: FilePath -> IO (Maybe String)
 readFileNow = go where
-    readNow :: Handle -> IO [String]
-    readNow my_handle = do
+    readContentNow :: Handle -> IO [String]
+    readContentNow my_handle = do
         my_handle_is_eof <- hIsEOF my_handle
         if my_handle_is_eof
             then return []
             else do
                 content <- hGetLine my_handle
-                contents <- readNow my_handle
+                contents <- readContentNow my_handle
                 content `seq` return (content : contents)
     go :: FilePath -> IO (Maybe String)
     go path = do
         my_handle <- openFile path ReadMode
         my_handle_is_open <- hIsOpen my_handle
         my_handle_is_okay <- if my_handle_is_open then hIsReadable my_handle else return False
-        maybe_contents <- if my_handle_is_okay then fmap (callWithStrictArg Just) (readNow my_handle) else return Nothing
-        hClose my_handle
-        return (fmap unlines maybe_contents)
-
-writeFileNow :: OStreamCargo a => FilePath -> a -> IO Bool
-writeFileNow path = go where
-    go :: OStreamCargo a => a -> IO Bool
-    go content = do
-        my_handle <- openFile path WriteMode
-        my_handle_is_open <- hIsOpen my_handle
-        my_handle_is_okay <- if my_handle_is_open then hIsWritable my_handle else return False
         if my_handle_is_okay
             then do
-                my_handle << content << Flush
-                return ()
-            else return ()
-        hClose my_handle
-        return my_handle_is_okay
+                contents <- readContentNow my_handle
+                callWithStrictArg (return . Just) (unlines contents)
+            else return Nothing
+
+writeFileNow :: OStreamCargo a => FilePath -> a -> IO Bool
+writeFileNow path content = do
+    my_handle <- openFile path WriteMode
+    my_handle_is_open <- hIsOpen my_handle
+    my_handle_is_okay <- if my_handle_is_open then hIsWritable my_handle else return False
+    when my_handle_is_okay $ do
+        my_handle << content << Flush
+        return ()
+    well_printed <- if my_handle_is_okay then hIsWritable my_handle else return False
+    hClose my_handle
+    return well_printed
