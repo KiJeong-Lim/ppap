df9fc8dd9e8746da4c4e124925f356e7b21e4b70
KiJeong-Lim <gijungdduk@naver.com>
2021-10-31 13:32:53 +0900

    .

--- src/Jasmine/Solver/Presburger/Test.hs
+++ src/Jasmine/Solver/Presburger/Test.hs
@@ -5,24 +5,34 @@ import Z.Algo.Function
 import Z.System.Shelly
 
 testPresburger :: IO ()
-testPresburger = mapM_ (shelly . runCase . getCase) [1 .. 12] where
+testPresburger = mapM_ (shelly . testCase . getCase) [1 .. 12] where
     getCase :: Int -> MyPresburgerFormulaRep
-    getCase 1 = (AllF 1 (AllF 2 (EqnF (Plus (IVar 1) (IVar 2)) (Plus (IVar 2) (IVar 1)))))
-    getCase 2 = (AllF 1 (LeqF (IVar 1) (IVar 1)))
-    getCase 3 = (AllF 1 (AllF 2 (NegF (ConF (LeqF (IVar 1) (IVar 2)) (GtnF (IVar 1) (IVar 2))))))
-    getCase 4 = (AllF 1 (AllF 2 (ImpF (ConF (LeqF (IVar 1) (IVar 2)) (LeqF (IVar 2) (IVar 1))) (EqnF (IVar 1) (IVar 2)))))
-    getCase 5 = (AllF 1 (AllF 2 (AllF 3 (ImpF (ConF (LeqF (IVar 1) (IVar 2)) (LeqF (IVar 2) (IVar 3))) (LeqF (IVar 1) (IVar 3))))))
-    getCase 6 = (AllF 1 (NegF (LtnF (IVar 1) (IVar 1))))
-    getCase 7 = (AllF 1 (ImpF (LtnF (Zero) (IVar 1)) (LtnF (IVar 1) (Plus (IVar 1) (IVar 1)))))
-    getCase 8 = (AllF 1 (AllF 2 (LeqF (IVar 1) (Plus (IVar 1) (IVar 2)))))
-    getCase 9 = (AllF 1 (AllF 2 (LeqF (IVar 2) (Plus (IVar 1) (IVar 2)))))
-    getCase 10 = (AllF 1 (AllF 2 (IffF (LeqF (Plus (Succ (Zero)) (IVar 2)) (Plus (IVar 1) (IVar 2))) (NegF (EqnF (IVar 1) (Zero))))))
-    getCase 11 = (AllF 1 (AllF 2 (LeqF (Plus (Succ (Zero)) (IVar 2)) (Plus (IVar 1) (IVar 2)))))
-    getCase 12 = (ExsF 1 (AllF 2 (LtnF (IVar 2) (IVar 1))))
-    runCase :: MyPresburgerFormulaRep -> String
-    runCase f
-        | isSentence f = "Presburger> " ++ (if isInTheory f then "The formula ``" ++ shows f "\'\' is a true sentence." else "The formula ``" ++ shows f "\'\' is a false sentence.")
-        | otherwise = "Presburger> " ++ "The formula ``" ++ shows f "\'\' is not a sentence."
+    getCase = at
+        [ (AllF 1 (AllF 2 (EqnF (Plus (IVar 1) (IVar 2)) (Plus (IVar 2) (IVar 1)))))
+        , (AllF 1 (LeqF (IVar 1) (IVar 1)))
+        , (AllF 1 (AllF 2 (NegF (ConF (LeqF (IVar 1) (IVar 2)) (GtnF (IVar 1) (IVar 2))))))
+        , (AllF 1 (AllF 2 (ImpF (ConF (LeqF (IVar 1) (IVar 2)) (LeqF (IVar 2) (IVar 1))) (EqnF (IVar 1) (IVar 2)))))
+        , (AllF 1 (AllF 2 (AllF 3 (ImpF (ConF (LeqF (IVar 1) (IVar 2)) (LeqF (IVar 2) (IVar 3))) (LeqF (IVar 1) (IVar 3))))))
+        , (AllF 1 (NegF (LtnF (IVar 1) (IVar 1))))
+        , (AllF 1 (ImpF (LtnF (Zero) (IVar 1)) (LtnF (IVar 1) (Plus (IVar 1) (IVar 1)))))
+        , (AllF 1 (AllF 2 (LeqF (IVar 1) (Plus (IVar 1) (IVar 2)))))
+        , (AllF 1 (AllF 2 (LeqF (IVar 2) (Plus (IVar 1) (IVar 2)))))
+        , (AllF 1 (AllF 2 (IffF (LeqF (Plus (Succ (Zero)) (IVar 2)) (Plus (IVar 1) (IVar 2))) (NegF (EqnF (IVar 1) (Zero))))))
+        , (AllF 1 (AllF 2 (LeqF (Plus (Succ (Zero)) (IVar 2)) (Plus (IVar 1) (IVar 2)))))
+        , (ExsF 1 (AllF 2 (LtnF (IVar 2) (IVar 1))))
+        ]
+    testCase :: MyPresburgerFormulaRep -> String
+    testCase f = "Presburger> " ++ testCaseAux1 (isSentence f) where
+        testCaseAux1 :: Bool -> String
+        testCaseAux1 f_is_a_sentence
+            | not f_is_a_sentence = "The formula ``" ++ shows f "\'\' is not a sentence."
+            | otherwise = testCaseAux2 (isInTheory f)
+        testCaseAux2 :: Bool -> String
+        testCaseAux2 f_is_a_theorem
+            | f_is_a_theorem = "The formula ``" ++ shows f "\'\' is a true sentence."
+            | otherwise = "The formula ``" ++ shows f "\'\' is a false sentence."
+    at :: [a] -> Int -> a
+    at xs i = xs !! (i + 1)
     isSentence :: MyPresburgerFormulaRep -> Bool
     isSentence = null . getFVs
     isInTheory :: MyPresburgerFormulaRep -> Bool
