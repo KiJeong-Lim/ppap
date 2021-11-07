fdd1b8004f01351eb184acab38673b5869031d54
KiJeong-Lim <gijungdduk@naver.com>
2021-11-08 07:36:12 +0900

    .

--- src/Z/Algo/Sorting.hs
+++ src/Z/Algo/Sorting.hs
@@ -6,54 +6,54 @@ import Control.Monad.Trans.Reader
 import Data.Functor.Identity
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
+import Z.Algo.Function
+import Z.Utils
 
 type IsCircular = Bool
 
 type IncreasingOrdering a = a -> a -> Bool
 
-getTSortedSCCs :: Ord node => Map.Map node (Set.Set node) -> [(IsCircular, Set.Set node)]
+type DigraphOf vertices = Map.Map vertices (Set.Set vertices)
+
+getTSortedSCCs :: Ord node => DigraphOf node -> [(IsCircular, Set.Set node)]
 getTSortedSCCs = runIdentity . go where
-    whenMaybe :: Applicative m => Bool -> m a -> m (Maybe a)
-    whenMaybe cond m = if cond then pure Just <*> m else pure Nothing
+    when' :: (Functor m, Monoid a) => Bool -> m a -> m a
+    when' cond = if cond then id else fmap (const mempty)
     sortByRel :: Ord node => [node] -> StateT (Set.Set node) (ReaderT (node -> [node]) Identity) [node]
     sortByRel [] = return []
     sortByRel (cur : nexts) = do
         visteds <- get
-        maybe_ges <- whenMaybe (not (cur `Set.member` visteds)) $ do
+        ges <- when' (not (cur `Set.member` visteds)) $ do
             put (Set.insert cur visteds)
             rel <- lift ask
             gts <- sortByRel (rel cur)
             return (cur : gts)
         lts <- sortByRel nexts
-        return (maybe lts (\ges -> lts ++ ges) maybe_ges)
+        return (lts ++ ges)
     splitByRel :: Ord node => [node] -> StateT (Set.Set node) (ReaderT (node -> [node]) Identity) [(IsCircular, Set.Set node)]
     splitByRel [] = return []
     splitByRel (cur : nexts) = do
         visteds <- get
-        maybe_ges <- whenMaybe (not (cur `Set.member` visteds)) (sortByRel [cur])
-        sets <- splitByRel nexts
-        return (maybe sets (\ges -> (cur `elem` ges, Set.fromAscList ges) : sets) maybe_ges)
+        cur_res <- when' (not (cur `Set.member` visteds)) $ do
+            ges <- sortByRel [cur]
+            return (one (cur `elem` ges, Set.fromAscList ges))
+        next_res <- splitByRel nexts
+        return (cur_res ++ next_res)
     go :: Ord node => Map.Map node (Set.Set node) -> Identity [(IsCircular, Set.Set node)]
-    go getDigraph = do
-        let getVertices = Set.toAscList (Map.keysSet getDigraph)
-            getOuts node = Set.toAscList (maybe (error "In `Z.Algo.Sorting.getTSortedSCCs':\n  an-element-is-out-of-domain.") id (Map.lookup node getDigraph))
-            getIns node = [ node' | (node', nodes) <- Map.toAscList getDigraph, node `Set.member` nodes ]
-        (sortedVertices, _) <- runReaderT (runStateT (sortByRel getVertices) Set.empty) getOuts
-        (sortedSCCs, _) <- runReaderT (runStateT (splitByRel getVertices) Set.empty) getIns
+    go given_digraph = do
+        let nodes = Set.toAscList (foldr Set.union (Map.keysSet given_digraph) (Map.elems given_digraph))
+            froms = maybe [] Set.toAscList . flip Map.lookup given_digraph
+            intos = Set.toAscList . Map.keysSet . flip Map.filter given_digraph . Set.member
+        (sortedVertices, _) <- runReaderT (runStateT (sortByRel nodes) Set.empty) froms
+        (sortedSCCs, _) <- runReaderT (runStateT (splitByRel nodes) Set.empty) intos
         return sortedSCCs
 
 sortByMerging :: IncreasingOrdering a -> [a] -> [a]
 sortByMerging = go where
     go :: IncreasingOrdering a -> [a] -> [a]
-    go leq xs
-        | n > 0 = case splitAt n xs of
-            (ys, zs) -> merge leq (go leq ys) (go leq zs)
-        | otherwise = xs
-        where
-            n :: Int
-            n = length xs `div` 2
+    go leq xs = case length xs `div` 2 of
+        0 -> xs
+        n -> uncurry (merge leq) . (go leq <^> go leq) $ (splitAt n xs)
     merge :: IncreasingOrdering a -> [a] -> [a] -> [a]
-    merge leq (y : ys) (z : zs)
-        | y `leq` z = y : merge leq ys (z : zs)
-        | otherwise = z : merge leq (y : ys) zs
+    merge leq (y : ys) (z : zs) = if y `leq` z then y : merge leq ys (z : zs) else z : merge leq (y : ys) zs
     merge leq ys zs = ys ++ zs
