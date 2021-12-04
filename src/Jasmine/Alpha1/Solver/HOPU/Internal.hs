88b9985e9a56c3c1263726720baaa3fba6a715f2
KiJeong-Lim <gijungdduk@naver.com>
2021-12-04 19:29:06 +0900

    comment on flex rigid

--- src/Jasmine/Alpha1/Solver/HOPU/Internal.hs
+++ src/Jasmine/Alpha1/Solver/HOPU/Internal.hs
@@ -33,52 +33,64 @@ import Z.Utils
 
 {- ${Term} is a L_lambda-pattern in ${Env}
 (1) RULE[ higher-order pattern ]
-    X is a flex var.
-    env.evalref X == none.
-    Every element of zs is a de-bruijn index or a rigid var.
-    For any rigid var c in zs, env.socpe X < env.scope c.
-    ========================================================
-    app(X, zs) is a L_lambda-pattern in env.
+    > X is a flex var.
+    > env.evalref X == none.
+    > Every element of zs is a de-bruijn index or a rigid var.
+    > For any rigid var c in zs, env.socpe X < env.scope c.
+    ==========================================================
+    > app(X, zs) is a L_lambda-pattern in env.
 -}
 
 {- ${LVar} +-> ${Term} yields ${Env} ~~> ${Env}
 (1) RULE[ update env by subst ]
-    X is a flex var.
-    env.evalref X == none.
-    X is not a member of FreeLVs(t).
-    env'.evalref v
-        = some t,        if v ==_{alpha} X;
-        = env.evalref v, otherwise.
-    env'.scope v
-        = min (env.scope X) (env.scope v), if v is a member of FreeLVs(t);
-        = env.scope v,                     otherwise.
+    > X is a flex var.
+    > env.evalref X == none.
+    > X is not a member of FreeLVs(t).
+    > env'.evalref v
+    >   = some t,        if v ==_{alpha} X;
+    >   = env.evalref v, otherwise.
+    > env'.scope v
+    >   = min (env.scope X) (env.scope v), if v is a member of FreeLVs(t);
+    >   = env.scope v,                     otherwise.
     ======================================================================
-    X +-> t yields env ~~> env'.
+    > X +-> t yields env ~~> env'.
 -}
 
 {- ${Env} ~~> MkRef[ ${Term} := ${Term} ] ~~> ${Env} with { newQs = ${Constraints} }
-(1) RULE[ flex-flex common head ]
-    app(X, [a_1 .. a_n]) is a L_lambda-pattern in env.
-    app(X, [b_1 .. b_(n + l)]) is a L_lambda-pattern in env.
-    env.evalref X == none.
-    as = [{| a_1, 0, l, [] |} .. {| a_n, 0, l, [] |}] ++ [#(l - 1) .. #0].
-    zs = [ #(n + l - i) | i <- [1 .. n + l], as !! (i - 1) == b_i ].
-    env' = env
-        |> new flex var H = { evalref = none, scope = env.scope X }
-        let t = lam(n + l). app(H, zs) in
-        |> update this.evalref X = (if t ==_{beta} eta(l, X) then none else some t).
-    =========================================================================================================
-    env ~~> MkRef[ app(X, [a_1 .. a_n]) := lam(l). app(X, [b_1 .. b_(n + l)]) ] ~~> env' with { newQs = [] }.
+(1) RULE[ flex-flex common head case ]
+    > app(X, [a_1 .. a_n]) is a L_lambda-pattern in env.
+    > app(X, [b_1 .. b_(n + l)]) is a L_lambda-pattern in env.
+    > env.evalref X == none.
+    > as = [{| a_1, 0, l, [] |} .. {| a_n, 0, l, [] |}] ++ [#(l - 1) .. #0].
+    > zs = [ #(n + l - i) | i <- [1 .. n + l], as !! (i - 1) == b_i ].
+    > env' = env
+    >   |> new flex var H = { evalref = none, scope = env.scope X }
+    >   let t = lam(n + l). app(H, zs) in
+    >   |> update this.evalref X = (if t ==_{beta} eta(l, X) then none else some t).
+    ===========================================================================================================
+    > env ~~> MkRef[ app(X, [a_1 .. a_n]) := lam(l). app(X, [b_1 .. b_(n + l)]) ] ~~> env' with { newQs = [] }.
 (2) RULE[ binding ]
-    app(X, [a_1 .. a_l]) is a L_lambda-pattern in env
-    The head of rhs is not X.
-    env.evalref X == none.
-    env |= Bind_{0}[ app(X, [a_1 .. a_l]) +-> rhs ] = t if probs satisfied.
-    X +-> lam(l). t yields env ~~> env'.
+    > app(X, [a_1 .. a_n]) is a L_lambda-pattern in env
+    > The head of t is not X.
+    > env.evalref X == none.
+    > env |= Bind_{0}[ app(X, [a_1 .. a_n]) +-> t ] = s, if probs satisfied.
+    > X +-> lam(n). s yields env ~~> env'.
     =============================================================================
-    env ~~> MkRef[ app(X, [a_1 .. a_l]) := rhs ] ~~> env' with { newQs = probs }.
+    > env ~~> MkRef[ app(X, [a_1 .. a_n]) := t ] ~~> env' with { newQs = probs }.
 -}
 
-{- ${Env} |= Bind_{${Nat}}[ ${Term} +-> ${Term} ] = {$Term} if ${Constraints} satisfied.
-
+{- ${Env} |= Bind_{${Nat}}[ ${Term} +-> ${Term} ] = ${Term}, if ${Constraints} satisfied.
+(1) RULE[ BindLam ]
+    > env |= Bind_{l + m}[ app(X, [a_1 .. a_n]) +-> t ] = s if probs satisfied.
+    > m is a positive integer.
+    =======================================================================================
+    > env |= Bind_{l}[ app(X, [a_1 .. a_n]) +-> lam(m). t ] = lam(m). s if probs satisfied.
+(2) RULE[ FlexRigid ]
+    > as = [{| a_1, 0, l, [] |} .. {| a_n, 0, l, [] |}] ++ [#(l - 1) .. #0].
+    > r' = r,  if r is a rigid var such that env.scope r < env.scope X;
+    >    = #i, if r is a de-bruijn index with r ==_{alpha} as !! i.
+    > For k in [1 .. m],
+    > env |= Bind_{l}[ app(X, [a_1 .. a_n]) +-> t_k ] = s_k if probs_k satisfied.
+    ====================================================================================================================================
+    > env |= Bind_{l}[ app(X, [a_1 .. a_n]) +-> app(r, [t_1 .. t_m]) ] = app(r', [s_1 .. s_m]) if concat [probs_1 .. probs_m] satisfied.
 -}
