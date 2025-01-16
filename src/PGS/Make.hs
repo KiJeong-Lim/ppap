3e9e69cf0161f911f0226c352bcbaf6e09a8f02a
KiJeong-Lim <gijungdduk@naver.com>
2025-01-16 20:12:32 +0900

    .

--- src/PGS/Make.hs
+++ src/PGS/Make.hs
@@ -247,7 +247,7 @@ makeCollectionAndLALR1Parser (CFGrammar start terminals productions) = theResult
             Just tss -> tss <> getFirstOf syms
         _Read :: Map.Map (ParserS, NSym) (Set.Set TSym)
         _Read = Map.fromList
-            [ ((p, _A), Set.fromList [ t | Just t <- Set.toList $ unTerminalSet (getFirstOf alpha2) ])
+            [ ((p, _A), Set.fromList [ t | Just t <- Set.toList (unTerminalSet (getFirstOf alpha2)) ])
             | (items, p) <- Map.toList (getVertices getCannonical0)
             , LR0Item _B alpha1 (NS _A : alpha2) <- Set.toList items
             ]
@@ -263,11 +263,11 @@ makeCollectionAndLALR1Parser (CFGrammar start terminals productions) = theResult
             my_F' (p, _A) = _Read Map.! (p, _A)
             my_R :: (ParserS, NSym) -> (ParserS, NSym) -> Bool
             my_R (p, _A) (q, _B) = or
-                [ Nothing `Set.member` unTerminalSet (getFirstOf alpha2) && delta' p alpha1 == Just q
+                [ Nothing `Set.member` unTerminalSet (getFirstOf alpha2) && delta' q alpha1 == Just p
                 | (items, p') <- Map.toList (getVertices getCannonical0)
                 , p == p'
                 , LR0Item _B' alpha1 (NS _A' : alpha2) <- Set.toList items
-                , _A == _A'
+                , _A == _A' && _B == _B'
                 ]
         _LA :: Map.Map (ParserS, ProductionRule) (Set.Set TSym)
         _LA = Map.fromList
