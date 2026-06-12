a89e4771ecd1d483c048afff7bea21a4e5eae170
임기정 <68202031+KiJeong-Lim@users.noreply.github.com>
2026-06-12 17:27:26 +0900

    Refactor prettyjson function for improved formatting

--- src/Json/JsonViewer.hs
+++ src/Json/JsonViewer.hs
@@ -6,29 +6,30 @@ import Json.JsonParser
 import Y.Base
 
 prettyjson :: ValueRep -> String
-prettyjson js_input
-    = case showsjson 0 js_input "" of
-        '\n' : str -> str
-        str -> str
-    where
-        tabjson :: Indentation -> Indentation
-        tabjson space = space + 4
-        showsjsnum :: NumberRep -> ShowS
-        showsjsnum (NumberRep_integer n) = strstr n
-        showsjsnum (NumberRep_fraction q) = strstr q
-        showsjsnum (NumberRep_exponent x) = strstr x
-        showsobject :: Indentation -> ObjectRep -> ShowS
-        showsobject space [] = strstr "{}"
-        showsobject space ((field1, val1) : elements) = strcat
-            [ nl . pindent space . strstr "{ " . strstr field1 . strstr " : " . showsjson (tabjson space) val1
-            , strcat $ map (uncurry $ \field -> \val -> nl . pindent space . strstr ", " . strstr field . strstr " : " . showsjson (tabjson space) val) elements
-            , nl . pindent space . strstr "}"
-            ]
-        showsjson :: Indentation -> ValueRep -> ShowS
-        showsjson space (ValueRep_object obj) = showsobject space obj
-        showsjson space (ValueRep_array ary) = plist space (map (showsjson (tabjson space)) ary)
-        showsjson space (ValueRep_string str) = strstr str
-        showsjson space (ValueRep_number num) = showsjsnum num
-        showsjson space (ValueRep_true) = strstr "true"
-        showsjson space (ValueRep_false) = strstr "false"
-        showsjson space (ValueRep_null) = strstr "null"
+prettyjson js_input = cut (showsjson 0 0 js_input "") where
+    tabjson :: Indentation -> Indentation
+    tabjson space = space + 2
+    showsjsnum :: NumberRep -> ShowS
+    showsjsnum (NumberRep_integer n) = strstr n
+    showsjsnum (NumberRep_fraction q) = strstr q
+    showsjsnum (NumberRep_exponent x) = strstr x
+    showsobject :: Precedence -> Indentation -> ObjectRep -> ShowS
+    showsobject prec space [] = strstr "{}"
+    showsobject prec space ((field1, val1) : elements) = strcat
+        [ (if prec > 0 then nl . pindent space else id) . strstr "{ " . addField field1. showsjson 1 (tabjson space) val1
+        , strcat $ map (uncurry $ \field -> \val -> nl . pindent space . strstr ", " . addField field . showsjson 1 (tabjson space) val) elements
+        , nl . pindent space . strstr "}"
+        ]
+    showsjson :: Precedence -> Indentation -> ValueRep -> ShowS
+    showsjson prec space (ValueRep_object obj) = showsobject prec space obj
+    showsjson prec space (ValueRep_array ary) = plist space (map (showsjson 0 (tabjson space)) ary)
+    showsjson prec space (ValueRep_string str) = strstr str
+    showsjson prec space (ValueRep_number num) = showsjsnum num
+    showsjson prec space (ValueRep_true) = strstr "true"
+    showsjson prec space (ValueRep_false) = strstr "false"
+    showsjson prec space (ValueRep_null) = strstr "null"
+    cut :: String -> String
+    cut ('\n' : str) = str
+    cut str = str
+    addField :: String -> ShowS
+    addField field = strstr field . strstr ": "
